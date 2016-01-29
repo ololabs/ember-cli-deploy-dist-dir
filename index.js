526564a76cdd5fdd75dc8796b2d8928ce63c3d1d
twaggs <tom.waggoner@gmail.com>
2016-01-29 16:45:21 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-/* jshint node: true */
-'use strict';
+var BasePlugin = require('ember-cli-deploy-plugin');
+var glob  = require('glob');
 
 module.exports = {
-  name: 'ember-cli-deploy-dist-dir'
+  name: 'ember-cli-deploy-dist-dir',
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = BasePlugin.extend({
+      name: options.name,
+
+      defaultConfig: {
+        filePattern: '**/**/*',
+        distDir: 'tmp/deploy-dist'
+      },
+
+      willBuild: function(context) {
+		var distDir = this.readConfig('distDir');
+		var filePattern = this.readConfig('filePattern');
+
+        this.log('Setting dist files and directory');
+
+        return {
+          distDir: distDir,
+          distFiles: glob.sync(filePattern, { nonull: false, nodir: true, cwd: distDir })
+        };
+      }
+    });
+
+    return new DeployPlugin();
+  }
 };
